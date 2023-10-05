f79cc71ab4733f7b5396e6056a13ae878ef8d30b
seatwiggy <93173836+seatwiggy@users.noreply.github.com>
2023-10-05 09:13:24 -0600

    Empty webcrawler project Compose file with kafka

---
+++ webcrawler/src/test/java/sjoquist/mathew/capstone/webcrawler/WebcrawlerApplicationTests.java
@@ -0,0 +1,10 @@
+package sjoquist.mathew.capstone.webcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcrawlerApplicationTests {
+	@Test
+	void contextLoads() {}
+}
