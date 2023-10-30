97be76535665d7ec40b882a141c8cbcf3886822a
seatwiggy <93173836+seatwiggy@users.noreply.github.com>
2023-10-30 10:05:41 -0600

    fixed special tokens in query parser

--- queryparser/src/main/java/sjoquist/mathew/capstone/queryparser/controllers/QueryController.java
+++ queryparser/src/main/java/sjoquist/mathew/capstone/queryparser/controllers/QueryController.java
@@ -15,12 +15,18 @@ public class QueryController {
         return ResponseEntity.ok().body(parse(query));
     }
 
+    /**
+     * Takes a user provided string and parses it into an endpoint for the website retrieval service
+     * @param query The user provided string to parse
+     * @return The endpoint to query the search engine with
+     */
     public static String parse(String query) {
         List<String> tokens = Stream.of(query.split("\\s+")).filter(s -> !s.isBlank()).map(String::toLowerCase).toList();
         List<String> specialTokens = tokens.stream().filter(s -> s.startsWith("-") || s.startsWith("url:")).toList();
         tokens.removeAll(specialTokens);
         StringBuilder endpoint = new StringBuilder("/webpages/search?");
 
+        // Add all regular tokens
         endpoint.append("text=");
         for (String token : tokens) {
             if (token.startsWith(query))
@@ -28,12 +34,24 @@ public class QueryController {
             endpoint.deleteCharAt(endpoint.length() - 1);
         }
 
-        for (String token : specialTokens) {
-            if (token.startsWith("-"))
-                endpoint.append("&exclude=").append(token.substring(1));
-            else if (token.startsWith("url:"))
-                endpoint.append("&url=").append(token.substring(4));
+        // Add all excluded tokens
+        if (specialTokens.stream().anyMatch(s -> s.startsWith("-"))) {
+            endpoint.append("&exclude=");
+            for (String token : specialTokens.stream().filter(s -> s.startsWith("-")).toList()) {
+                endpoint.append(token.substring(1)).append("+");
+            }
+            endpoint.deleteCharAt(endpoint.length() - 1);
         }
+
+        // Add all url tokens
+        if (specialTokens.stream().anyMatch(s -> s.startsWith("url:"))) {
+            endpoint.append("&url=");
+            for (String token : specialTokens.stream().filter(s -> s.startsWith("url:")).toList()) {
+                endpoint.append(token.substring(4)).append("+");
+            }
+            endpoint.deleteCharAt(endpoint.length() - 1);
+        }
+
         return endpoint.toString();
     }
 }
--- queryparser/src/test/java/sjoquist/mathew/capstone/queryparser/QueryparserApplicationTests.java
+++ queryparser/src/test/java/sjoquist/mathew/capstone/queryparser/QueryparserApplicationTests.java
@@ -3,22 +3,26 @@ package sjoquist.mathew.capstone.queryparser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import sjoquist.mathew.capstone.queryparser.controllers.QueryController;
 
 @SpringBootTest
 class QueryparserApplicationTests {
-
 	@SuppressWarnings("java:S2699")
 	@Test
 	void contextLoads() {
 	}
 
-	@Test
-	void simpleQuery() {
-		String query = "hello world";
-		String expected = "/webpages/search?text=hello+world";
+	@ParameterizedTest
+	@CsvSource({
+			"hello world,/webpages/search?text=hello+world,",
+			"hello world url:google.com url:duck.com,/webpages/search?text=hello+world&url=google.com+dock.com,",
+			"hello world -goodbye -later,/webpages/search?text=hello+world&exclude=goodbye+later,"
+	})
+	void testQueryParsing(String query, String expected) {
 		String actual = QueryController.parse(query);
 		assertEquals(expected, actual);
 	}
